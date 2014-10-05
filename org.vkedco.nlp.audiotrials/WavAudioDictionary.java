1e9b93e825ed1ddbc0b02edb7c3d8c40850e9510
vkedco <vladimir.kulyukin@gmail.com>
2014-10-04 23:20:22 -0600

    Update WavAudioDictionary.java
    added buildSegmentDictionary()

--- org.vkedco.nlp.audiotrials/WavAudioDictionary.java
+++ org.vkedco.nlp.audiotrials/WavAudioDictionary.java
@@ -1,162 +1,160 @@
-package org.vkedco.nlp.audiotrials;
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.vkedco.audio.wavlib;
 
-// Bugs to vladimir dot kulyukin at gmail dot com.
-
+/**
+ *
+ * @author vladimir kulyukin
+ * 
+ * bugs to vladimir dot kulyukin at gmail dot com
+ */
 import java.io.File;
 import java.io.FilenameFilter;
-import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Map.Entry;
+import java.util.Set;
 import java.util.TreeMap;
 
-public class WavAudioDictionary {
+/*
+ * buildAudioPacketDictionary method works as follows: each wav file is split into non-silence chunks.
+ * Each non-silence chunk is represented as an array of segments. A segment is
+ * a double[] of even length. The algorithm should accept zcr_threshold and
+ * amp_thresh.
+ * When an input file is given, the algorithm splits it into non-silence chunks.
+ * Then it goes through the dictionary and does the DTW match on the 1st chunks 
+ * of each entry. Those entries that pass the threshold, continue to the 2nd round.
+ * The algorithm can be parameterized by how many chunks need to match before
+ * the final match is returned.
+ * Another modification is to keep the entry in even when a certain number of
+ * mismatches has taken place.
+ * Another interesting modification is to select a random sample of packets
+ * from the input and match it against the same selection of packets from a
+ * dictionary entry. 
+ */
+
+public class WavDictionary {
+
+    static TreeMap<String, ArrayList<Double>> mLists = null;
+    static TreeMap<String, double[]> mArrays = null;
+    static TreeMap<String, ArrayList<double[]>> mSegments = null;
+
+    public static void addAudioAmpList(String word, ArrayList<Double> amp_list) {
+        mLists.put(word, amp_list);
+    }
+
+    public static void addAudioAmpArray(String word, double[] amp_ary) {
+        mArrays.put(word, amp_ary);
+    }
     
-    // change this directory to where your audio files are stored.
-    static final String AUDIO_DIR = 
-            "C:/Users/Vladimir/Dropbox/MyShare/TEACHING/CS6890_NLP/support_materials/audio_files/";
+    public static void addAudioArrayOfSegments(String word, ArrayList<double[]> arrayOfSegments) {
+        mSegments.put(word, arrayOfSegments);
+    }
     
-    static final String AUDIO_DICTIONARY = AUDIO_DIR + "wav_audio_dictionary/";
-
-    static TreeMap<String, ArrayList<Double>> mWords = null;
-
-    static {
-        mWords = new TreeMap<String, ArrayList<Double>>();
+    public static void displayAudioArraySegments() {
+        for(Entry<String, ArrayList<double[]>> e : mSegments.entrySet()) {
+            System.out.println(e.getKey() + " : " + e.getValue().size());
+        }
     }
 
-    static void addAudioAmpList(String word, ArrayList<Double> amp_list) {
-        mWords.put(word, amp_list);
+    public static ArrayList<Double> getAmpList(String word) {
+        return mLists.get(word);
     }
 
-    static ArrayList<Double> getAmpList(String word) {
-        return mWords.get(word);
+    public static double[] getAmpArray(String word) {
+        return mArrays.get(word);
+    }
+    
+    public static ArrayList<double[]> getSegmentList(String word) {
+        return mSegments.get(word);
     }
 
-    static void buildWavAudioDictionary(String dir_path, int ch_num, int frame_sample_size,
+    public static int getArrayDictionarySize() {
+        return mArrays.size();
+    }
+
+    public static int getListDictionarySize() {
+        return mLists.size();
+    }
+    
+    public TreeMap<String, ArrayList<double[]>> getSegments() {
+        return mSegments;
+    }
+    
+    public TreeMap<String, ArrayList<Double>> getLists() {
+        return mLists;
+    }
+    
+    public TreeMap<String, double[]> getArrays() {
+        return mArrays;
+    }
+    
+    public static void buildSegmentDictionary(String dir_path, int ch_num, int frame_sample_size,
             double zcr_thresh, double amp_thresh) {
+        mSegments = new TreeMap<>();
         File dir = new File(dir_path);
 
         if (dir.isDirectory()) {
             for (String f : dir.list(new FilenameFilter() {
-
                 public boolean accept(File directory, String fileName) {
                     return fileName.endsWith(".wav");
                 }
             })) {
                 System.out.println(f);
-                WavAudioDictionary.addAudioAmpList(f.substring(0, f.indexOf(".")),
-                        wavFileToNonSilenceAmpArrayList(dir_path + f, ch_num, frame_sample_size, zcr_thresh, amp_thresh));
+                WavDictionary.addAudioArrayOfSegments(f.substring(0, f.indexOf(".")),
+                        WavSpeech.wavFileToListOfNonSilenceSegments(dir_path + f, ch_num, 
+                                                    frame_sample_size, zcr_thresh, amp_thresh));
+            }
+        }
+        
+    }
+    
+    public static void buildListDictionary(String dir_path, int ch_num, int frame_sample_size,
+            double zcr_thresh, double amp_thresh) {
+        mLists = new TreeMap<>();
+        File dir = new File(dir_path);
+
+        if (dir.isDirectory()) {
+            for (String f : dir.list(new FilenameFilter() {
+                public boolean accept(File directory, String fileName) {
+                    return fileName.endsWith(".wav");
+                }
+            })) {
+                System.out.println(f);
+                WavDictionary.addAudioAmpList(f.substring(0, f.indexOf(".")),
+                        WavSpeech.wavFileToNonSilenceAmpArrayList(dir_path + f, ch_num, frame_sample_size, zcr_thresh, amp_thresh));
             }
         }
     }
 
-    static ArrayList<Double> wavFileToNonSilenceAmpArrayList(String wav_in_path,
-            int ch_num, int frame_sample_size,
+    public static void buildArrayDictionary(String dir_path, int ch_num, int frame_sample_size,
             double zcr_thresh, double amp_thresh) {
+        mArrays = new TreeMap<>();
+        File dir = new File(dir_path);
 
-        ArrayList<Double> amp_list = new ArrayList<Double>();
-
-        try {
-            int frames_read = 0;
-            double currZCR = 0;
-            double currAmp = 0.0;
-            WavFile inWavFile = WavFile.openWavFile(new File(wav_in_path));
-            long sample_rate = inWavFile.getSampleRate();
-            double normalizer = WavFileManip.convertFrameSampleSizeToSeconds((int) sample_rate, frame_sample_size);
-
-            // Get the number of audio channels in the wav file
-            int num_channels = inWavFile.getNumChannels();
-
-            System.out.println("WavFile's number of frames: " + inWavFile.getNumFrames());
-            System.out.println("WavFile's sample rate: " + inWavFile.getSampleRate());
-
-            // Create a buffer of frame_sample_size frames
-            double[][] buffer = new double[num_channels][frame_sample_size];
-
-            do {
-                frames_read = inWavFile.readFrames(buffer, frame_sample_size);
-                currZCR = ZeroCrossingRate.computeZCR01(buffer[ch_num], normalizer);
-                currAmp = WavFileManip.computeAvrgAbsAmplitude(buffer[ch_num]);
-
-                //if ( framesRead > 0 ) currFrameSampleNum++;
-
-                if (currZCR > zcr_thresh || currAmp > amp_thresh) {
-                    //System.out.println("frames_read == " + frames_read);
-                    if (frames_read > 0) {
-                        for (int i = 0; i < buffer[ch_num].length; i++) {
-                            //System.out.println("adding " + buffer[ch_num][i]);
-                            amp_list.add(new Double(buffer[ch_num][i]));
-                        }
-                    }
+        if (dir.isDirectory()) {
+            for (String f : dir.list(new FilenameFilter() {
+                public boolean accept(File directory, String fileName) {
+                    return fileName.endsWith(".wav");
                 }
-            } while (frames_read != 0);
-
-            inWavFile.close();
-
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (WavFileException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            })) {
+                System.out.println(f);
+                WavDictionary.addAudioAmpArray(f.substring(0, f.indexOf(".")),
+                        WavSpeech.wavFileToNonSilenceAmpArray(dir_path + f, ch_num, frame_sample_size, zcr_thresh, amp_thresh));
+            }
         }
-
-        return amp_list;
     }
     
-    static void calcium_against_calcium_test() {
-        ArrayList<Double> amp_list01 =
-                WavAudioDictionary
-                    .wavFileToNonSilenceAmpArrayList(AUDIO_DIR + "calcium.wav",
-                                                    0, 200, 1270.0, 0.005);
-
-        ArrayList<Double> amp_list02 = WavAudioDictionary.getAmpList("calcium");
-        IDTWSimilarity isim = new AbsModSim();
-        System.out.println("here we go");
-        double[][] cost_matrix2 = DTW.dp_dtw_2col_cost_matrix(amp_list01, amp_list02, isim);
-        System.out.println("Cost == " + cost_matrix2[amp_list01.size() - 1][1]);
+    public static Set<String> getListKeys() {
+        return mLists.keySet();
     }
 
-    static void fat_against_fat_test() {
-        ArrayList<Double> amp_list01 =
-                WavAudioDictionary
-                    .wavFileToNonSilenceAmpArrayList(AUDIO_DIR + "fat.wav",
-                                                     0, 200, 1270.0, 0.005);
-
-        ArrayList<Double> amp_list02 = WavAudioDictionary.getAmpList("fat");
-        IDTWSimilarity isim = new AbsModSim();
-        System.out.println("here we go");
-        double[][] cost_matrix2 = DTW.dp_dtw_2col_cost_matrix(amp_list01, amp_list02, isim);
-        System.out.println("Cost == " + cost_matrix2[amp_list01.size() - 1][1]);
+    public static Set<String> getArrayKeys() {
+        return mArrays.keySet();
     }
-
-    static void fat_against_fact_test() {
-        ArrayList<Double> amp_list01 =
-                WavAudioDictionary
-                    .wavFileToNonSilenceAmpArrayList(AUDIO_DIR + "fat.wav",
-                0, 200, 1270.0, 0.005);
-
-        ArrayList<Double> amp_list02 = WavAudioDictionary.getAmpList("fact");
-        IDTWSimilarity isim = new AbsModSim();
-        System.out.println("here we go");
-        double[][] cost_matrix2 = DTW.dp_dtw_2col_cost_matrix(amp_list01, amp_list02, isim);
-        System.out.println("Cost == " + cost_matrix2[amp_list01.size() - 1][1]);
-    }
-
-    static void calorie_against_calories_test() {
-        ArrayList<Double> amp_list01 =
-                WavAudioDictionary
-                    .wavFileToNonSilenceAmpArrayList(AUDIO_DIR + "calorie.wav",
-                0, 200, 1270.0, 0.005);
-
-        ArrayList<Double> amp_list02 = WavAudioDictionary.getAmpList("calories");
-        IDTWSimilarity isim = new AbsModSim();
-        System.out.println("here we go");
-        double[][] cost_matrix2 = DTW.dp_dtw_2col_cost_matrix(amp_list01, amp_list02, isim);
-        System.out.println("Cost == " + cost_matrix2[amp_list01.size() - 1][1]);
-    }
-
-    public static void main(String[] args) {
-        WavAudioDictionary.buildWavAudioDictionary(AUDIO_DICTIONARY, 0, 200, 1270.0, 0.005);
-        System.out.println(WavAudioDictionary.mWords.size());
-        WavAudioDictionary.calorie_against_calories_test();
-    }
+    
+    public static Set<String> getSegmentKeys() {
+        return mSegments.keySet();
+    } 
 }
