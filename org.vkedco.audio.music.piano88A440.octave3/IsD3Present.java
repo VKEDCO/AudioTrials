8bcb5459b37b9aacb9bb742d7c85acd4cdd36ab9
vkedco <vladimir.kulyukin@gmail.com>
2015-09-09 14:09:06 -0600

    Create IsD3Present.java

---
+++ org.vkedco.audio.music.piano88A440.octave3/IsD3Present.java
@@ -0,0 +1,32 @@
+package org.vkedco.audio.music.piano88A440.octave3;
+
+import org.vkedco.audio.event.detection.fourier.FourierAudioFreqEventDetector;
+import org.vkedco.audio.music.piano88A440.Piano88A440KeysAndFreqs;
+import org.vkedco.audio.music.piano88A440.Piano88A440NoteFrequencyRanges;
+
+ /*****************************
+ * @author Vladimir Kulyukin
+ *****************************
+ */
+
+public class IsD3Present extends FourierAudioFreqEventDetector {
+    
+    static final String MESSAGE     = "D3-RE3 Present ";
+    static final String DUAL_NAME   = "D3-RE3";
+    static final String LETTER_NAME = Piano88A440KeysAndFreqs.D3;
+    
+    public IsD3Present(double xlower, double xupper, double step, double cthresh, double athresh, double error_margin) {
+        super((int)(Piano88A440KeysAndFreqs.D3_FREQ - Piano88A440NoteFrequencyRanges.BAND), 
+              (int)(Piano88A440KeysAndFreqs.D3_FREQ + Piano88A440NoteFrequencyRanges.BAND), 
+                xlower, xupper, step, cthresh, athresh, error_margin);
+    }
+    
+    @Override
+    public String message() { return MESSAGE; }
+    
+    @Override
+    public String getDualName() { return DUAL_NAME; }
+    
+    @Override
+    public String getLetterName() { return LETTER_NAME; }
+}
