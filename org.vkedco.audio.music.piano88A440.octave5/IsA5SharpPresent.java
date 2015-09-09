8a95e318898a9ae62c1aacc7762fa7dc9e897e62
vkedco <vladimir.kulyukin@gmail.com>
2015-09-09 14:32:49 -0600

    Create IsA5SharpPresent.java

---
+++ org.vkedco.audio.music.piano88A440.octave5/IsA5SharpPresent.java
@@ -0,0 +1,33 @@
+package org.vkedco.audio.music.piano88A440.octave5;
+
+import org.vkedco.audio.event.detection.fourier.FourierAudioFreqEventDetector;
+import org.vkedco.audio.music.piano88A440.Piano88A440KeysAndFreqs;
+import org.vkedco.audio.music.piano88A440.Piano88A440NoteFrequencyRanges;
+
+ /*
+ *******************************
+ * @author Vladimir Kulyukin
+ *******************************
+ */
+
+public class IsA5SharpPresent extends FourierAudioFreqEventDetector {
+    
+    static final String MESSAGE     = "A5#-SOL5# Present ";
+    static final String DUAL_NAME   = "A5#-SOL5#";
+    static final String LETTER_NAME = Piano88A440KeysAndFreqs.A5_SHARP;
+    
+    public IsA5SharpPresent(double xlower, double xupper, double step, double cthresh, double athresh, double error_margin) {
+        super((int)(Piano88A440KeysAndFreqs.A5_SHARP_FREQ - Piano88A440NoteFrequencyRanges.BAND), 
+              (int)(Piano88A440KeysAndFreqs.A5_SHARP_FREQ + Piano88A440NoteFrequencyRanges.BAND), 
+                xlower, xupper, step, cthresh, athresh, error_margin);
+    }
+    
+    @Override
+    public String message() { return MESSAGE; }
+    
+    @Override
+    public String getDualName() { return DUAL_NAME; }
+    
+    @Override
+    public String getLetterName() { return LETTER_NAME; }
+}
