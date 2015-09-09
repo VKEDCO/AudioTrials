a6db6a37c603a2b108c251fdbf56f28c3dc361f3
vkedco <vladimir.kulyukin@gmail.com>
2015-09-09 14:35:53 -0600

    Create IsG5Present.java

---
+++ org.vkedco.audio.music.piano88A440.octave5/IsG5Present.java
@@ -0,0 +1,39 @@
+package org.vkedco.audio.music.piano88A440.octave5;
+
+import org.vkedco.audio.event.detection.fourier.FourierAudioFreqEventDetector;
+import org.vkedco.audio.music.piano88A440.Piano88A440KeysAndFreqs;
+import org.vkedco.audio.music.piano88A440.Piano88A440NoteFrequencyRanges;
+
+/**
+ *****************************
+ * @author Vladimir Kulyukin
+ *****************************
+ */
+public class IsG5Present extends FourierAudioFreqEventDetector {
+    
+    static final String MESSAGE      = "G5-SOL5 Present ";
+    static final String DUAL_NAME    = "G5-SOL5";
+    static final String LETTER_NAME  = Piano88A440KeysAndFreqs.G5;
+    
+    public IsG5Present(double xlower, double xupper, double step, double cthresh, double athresh, double error_margin) {
+        super((int)(Piano88A440KeysAndFreqs.G5_FREQ - Piano88A440NoteFrequencyRanges.BAND), 
+              (int)(Piano88A440KeysAndFreqs.G5_FREQ + Piano88A440NoteFrequencyRanges.BAND), 
+                xlower, xupper, step, cthresh, athresh, error_margin);
+    }
+    
+    @Override
+    public String message() {
+        return MESSAGE;
+    }
+    
+    @Override
+    public String getDualName() {
+        return DUAL_NAME;
+    }
+    
+    @Override
+    public String getLetterName() {
+        return LETTER_NAME;
+    }
+}
+
