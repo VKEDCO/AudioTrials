d4f47746c82e7dec33e920d8c65760e5646d9e00
vkedco <vladimir.kulyukin@gmail.com>
2015-09-09 14:26:54 -0600

    Create IsC4SharpPresent.java

---
+++ org.vkedco.audio.music.piano88A440.octave4/IsC4SharpPresent.java
@@ -0,0 +1,33 @@
+package org.vkedco.audio.music.piano88A440.octave4;
+
+import org.vkedco.audio.event.detection.fourier.FourierAudioFreqEventDetector;
+import org.vkedco.audio.music.piano88A440.Piano88A440KeysAndFreqs;
+import org.vkedco.audio.music.piano88A440.Piano88A440NoteFrequencyRanges;
+
+ /*
+ *****************************
+ * @author Vladimir Kulyukin
+ *****************************
+ */
+
+public class IsC4SharpPresent extends FourierAudioFreqEventDetector {
+    
+    static final String MESSAGE     = "D04#-C4# Present ";
+    static final String DUAL_NAME   = "D04#-C4#";
+    static final String LETTER_NAME = Piano88A440KeysAndFreqs.C4_SHARP;
+    
+    public IsC4SharpPresent(double xlower, double xupper, double step, double cthresh, double athresh, double error_margin) {
+        super((int)(Piano88A440KeysAndFreqs.C4_SHARP_FREQ - Piano88A440NoteFrequencyRanges.BAND), 
+              (int)(Piano88A440KeysAndFreqs.C4_SHARP_FREQ + Piano88A440NoteFrequencyRanges.BAND), 
+                xlower, xupper, step, cthresh, athresh, error_margin);
+    }
+    
+    @Override
+    public String message() { return MESSAGE; }
+    
+    @Override
+    public String getDualName() { return DUAL_NAME; }
+    
+    @Override
+    public String getLetterName() { return LETTER_NAME; }
+}
