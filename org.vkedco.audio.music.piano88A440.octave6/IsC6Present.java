b9b9923005ed101ad25e9c1199c82ecb433acdfb
vkedco <vladimir.kulyukin@gmail.com>
2015-09-09 14:38:50 -0600

    Create IsC6Present.java

---
+++ org.vkedco.audio.music.piano88A440.octave6/IsC6Present.java
@@ -0,0 +1,34 @@
+package org.vkedco.audio.music.piano88A440.octave6;
+
+import org.vkedco.audio.event.detection.fourier.FourierAudioFreqEventDetector;
+import org.vkedco.audio.music.piano88A440.Piano88A440KeysAndFreqs;
+import org.vkedco.audio.music.piano88A440.Piano88A440NoteFrequencyRanges;
+
+/**
+ *****************************
+ * @author Vladimir Kulyukin
+ *****************************
+ */
+
+public class IsC6Present extends FourierAudioFreqEventDetector {
+    
+    static final String MESSAGE     = "C6-DO6 Present ";
+    static final String DUAL_NAME   = "C6-DO6";
+    static final String LETTER_NAME = Piano88A440KeysAndFreqs.C6;
+    
+    public IsC6Present(double xlower, double xupper, double step, double cthresh, double athresh, double error_margin) {
+        super((int)(Piano88A440KeysAndFreqs.C6_FREQ - Piano88A440NoteFrequencyRanges.BAND), 
+              (int)(Piano88A440KeysAndFreqs.C6_FREQ + Piano88A440NoteFrequencyRanges.BAND), 
+                xlower, xupper, step, cthresh, athresh, error_margin);
+    }
+    
+    @Override
+    public String message() { return MESSAGE; }
+    
+    @Override
+    public String getDualName() { return DUAL_NAME; }
+    
+    @Override
+    public String getLetterName() { return LETTER_NAME; }
+    
+}
