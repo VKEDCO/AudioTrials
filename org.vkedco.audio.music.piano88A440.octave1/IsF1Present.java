7ef055a38f40d2c63da8f6bfedfdb410a9f1863d
vkedco <vladimir.kulyukin@gmail.com>
2015-09-09 13:56:32 -0600

    Create IsF1Present.java

---
+++ org.vkedco.audio.music.piano88A440.octave1/IsF1Present.java
@@ -0,0 +1,31 @@
+package org.vkedco.audio.music.piano88A440.octave1;
+
+import org.vkedco.audio.event.detection.fourier.FourierAudioFreqEventDetector;
+import org.vkedco.audio.music.piano88A440.Piano88A440KeysAndFreqs;
+import org.vkedco.audio.music.piano88A440.Piano88A440NoteFrequencyRanges;
+
+ /*****************************
+ * @author Vladimir Kulyukin
+ *****************************
+ */
+
+public class IsF1Present extends FourierAudioFreqEventDetector {
+    
+    static final String MESSAGE     = "F1-FA1 Present ";
+    static final String DUAL_NAME   = "F1-FA1";
+    static final String LETTER_NAME = Piano88A440KeysAndFreqs.F1;
+    public IsF1Present(double xlower, double xupper, double step, double cthresh, double athresh, double error_margin) {
+        super((int)(Piano88A440KeysAndFreqs.F1_FREQ - Piano88A440NoteFrequencyRanges.BAND), 
+              (int)(Piano88A440KeysAndFreqs.F1_FREQ + Piano88A440NoteFrequencyRanges.BAND), 
+                xlower, xupper, step, cthresh, athresh, error_margin);
+    }
+    
+    @Override
+    public String message() { return MESSAGE; }
+    
+    @Override
+    public String getDualName() { return DUAL_NAME; }
+    
+    @Override
+    public String getLetterName() { return LETTER_NAME; }
+}
