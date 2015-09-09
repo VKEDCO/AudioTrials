a91afcd9e0baa05f9d274f7d574094198323ac24
vkedco <vladimir.kulyukin@gmail.com>
2015-09-09 14:40:14 -0600

    Create IsE6Present.java

---
+++ org.vkedco.audio.music.piano88A440.octave6/IsE6Present.java
@@ -0,0 +1,32 @@
+package org.vkedco.audio.music.piano88A440.octave6;
+
+import org.vkedco.audio.event.detection.fourier.FourierAudioFreqEventDetector;
+import org.vkedco.audio.music.piano88A440.Piano88A440KeysAndFreqs;
+import org.vkedco.audio.music.piano88A440.Piano88A440NoteFrequencyRanges;
+
+/**
+ *****************************
+ * @author Vladimir Kulyukin
+ ***************************** 
+ */
+public class IsE6Present extends FourierAudioFreqEventDetector {
+    
+    static final String MESSAGE     = "E6-MI6 Present ";
+    static final String DUAL_NAME   = "E6-MI6";
+    static final String LETTER_NAME = Piano88A440KeysAndFreqs.E6;
+    
+    public IsE6Present(double xlower, double xupper, double step, double cthresh, double athresh, double error_margin) {
+        super((int)(Piano88A440KeysAndFreqs.E6_FREQ - Piano88A440NoteFrequencyRanges.BAND), 
+              (int)(Piano88A440KeysAndFreqs.E6_FREQ + Piano88A440NoteFrequencyRanges.BAND), 
+                xlower, xupper, step, cthresh, athresh, error_margin);
+    }
+    
+    @Override
+    public String message() { return MESSAGE; }
+    
+    @Override
+    public String getDualName() { return DUAL_NAME; }
+    
+    @Override
+    public String getLetterName() { return LETTER_NAME; }
+}
