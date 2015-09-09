9d4b2648db53e33d02acfa43886c29491465f67c
vkedco <vladimir.kulyukin@gmail.com>
2015-09-09 14:13:38 -0600

    Create Function.java

---
+++ org.vkedco.calc.utils/Function.java
@@ -0,0 +1,15 @@
+package org.vkedco.calc.utils;
+
+/**
+ *
+ * @author vladimir kulyukin
+ */
+public class Function {
+    
+    public Function() {}
+    public double v(double x) { return 0; }
+    public double[] generateRangeInterval(double[] domain_values) {
+        return null;
+    }
+    
+}
